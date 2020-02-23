dd6526e0fc955b40cd28f1627b19973b151946ff
Mueller <muj@awk.ch>
2020-02-23 22:48:23 +0100

    json request viewer

--- clab-bot/__init__.py
+++ clab-bot/__init__.py
@@ -1,4 +1,5 @@
 import logging
+import json
 
 import azure.functions as func
 
@@ -6,19 +7,10 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello {name}!")
-    else:
-        return func.HttpResponse(
-             "Please pass a name on the query string or in the request body",
-             status_code=400
-        )
+    return func.HttpResponse(json.dumps({
+        'method': req.method,
+        'url': req.url,
+        'headers': dict(req.headers),
+        'params': dict(req.params),
+        'get_body': req.get_body().decode()
+    }, indent=4))
